26cfa12e71519bb121851f3e8f4a084701244958
Nikheel Jain <nikheel.jain@shl.com>
2022-06-18 19:39:00 +0530

    adding

--- 02_FrontEnd/src/client/modules/MyTranscription.tsx
+++ 02_FrontEnd/src/client/modules/MyTranscription.tsx
@@ -3,22 +3,27 @@ import { Flex, Avatar, Card, CardHeader, CardBody, Text } from "@fluentui/react-
 import * as PropTypes from "prop-types";
 import { TranscriptionEntity } from "../Models/ModelEntities";
 
-const MyTranscription = (props) => (
-    <Card fluid selected>
-        <CardHeader>
-            <Flex gap= "gap.small" >
-                <Avatar name={props.transcripion.who}/>
-                <Flex column>
-                    <Text content={props.transcription.who} weight= "bold" />
-                    <Text content={new Date(Date.parse(props.transcription.when)).toLocaleString()} size= "small" />
+const MyTranscription = (props) =>{
+    console.log(props);
+    return (
+        <Card fluid selected>
+            <CardHeader>
+                <Flex gap= "gap.small" >
+                    <Avatar name={props.transcripion.who}/>
+                    <Flex column>
+                        <Text content={props.transcription.who} weight= "bold" />
+                        <Text content={new Date(Date.parse(props.transcription.when)).toLocaleString()} size= "small" />
+                    </Flex>
                 </Flex>
-            </Flex>
-        </CardHeader>
-        <CardBody>{props.transcription.text}
-            <b>Translation:</b>{props.transcription.translations}
-        </CardBody>
-    </Card>
-);
+            </CardHeader>
+            <CardBody>{props.transcription.text}
+                <b>Translation:</b>{props.transcription.translations}
+            </CardBody>
+        </Card>
+    );
+};
 
-MyTranscription.propTypes = { transcription: PropTypes.arrayOf(PropTypes.instanceOf(TranscriptionEntity)) };
+MyTranscription.propTypes = {
+    transcription: PropTypes.arrayOf(PropTypes.instanceOf(TranscriptionEntity))
+};
 export default MyTranscription;
