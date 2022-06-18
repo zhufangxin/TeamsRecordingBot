308e0a764c49675c39066e537e329811fcff2813
Nikheel Jain <nikheel.jain@shl.com>
2022-06-18 18:32:09 +0530

    adding

--- 02_FrontEnd/src/client/modules/InviteBot.tsx
+++ 02_FrontEnd/src/client/modules/InviteBot.tsx
@@ -11,16 +11,16 @@ const InviteBot = (props) => {
 
     const inviteBot = () => {
 
-        if (currentTranscriptionLang && currentTranslationLang){
+        if (currentTranscriptionLang && currentTranslationLang) {
             let lEndPoint = process.env.REACT_APP_BACKEND_API as string;
             lEndPoint = lEndPoint + "api/InviteBot";
-            const lBody = {JoinURL: props.currentJoinUrl ? encodeURI(props.currentJoinUrl) : "" , TranscriptionLanguage: currentTranscriptionLang, TranslationLanguages: [currentTranslationLang]};
+            const lBody = {JoinURL: props.currentJoinUrl ? encodeURI(props.currentJoinUrl) : "", TranscriptionLanguage: currentTranscriptionLang, TranslationLanguages: [currentTranslationLang]};
             console.log("Got invite bot endpoint: " + lEndPoint + ". Body is: " + JSON.stringify(lBody));
             fetch(lEndPoint, {
                 method: "POST",
                 headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json"
+                    Accept: "application/json" ,
+                    "Content-Type" : "application/json"
                 },
                 body: JSON.stringify(lBody)
             })
